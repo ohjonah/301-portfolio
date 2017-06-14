5134796c686315ac1d380e803ac65da1113fc6ef
ohjonah <ohjonah@gmail.com>
2017-06-14 10:57:16 -0700

    Added Project Template Section
    created project template section and div text overlay that will be filled dynamically using jQ and styled with CSS

--- js/app.js
+++ js/app.js
@@ -13,6 +13,33 @@ function Project(rawDataObj) {
   this.repoUrl = rawDataObj.repoUrl;
 }
 
++  $newArticle.removeClass('template');
+    if (!this.publishedOn) $newArticle.addClass('draft');
+    $newArticle.data('category', this.category);
+
+   /* TODO: Now use jQuery traversal and setter methods to fill in the rest
+   of the current template clone with properties from this particular Article instance.
+   We need to fill in:
+
+     1. author name,
+     2. author url,
+     3. article title,
+      4. article body, and
+      5. publication date. */
+
+ -    $newArticle.find('.byline a').html(this.author);
+ +  $newArticle.find('.byline a').html(this.author);
+ +  $newArticle.find('.byline a').attr('href', this.authorUrl);
+ +  $newArticle.find('h1').html(this.title);
+ +  $newArticle.find('.article-body').html(this.body);
+ +  $newArticle.find('time').attr('datetime', this.publishedOn);
+
+    // Display the date as a relative number of 'days ago'
+    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
+   $newArticle.append('<hr>');
+   return $newArticle;
+ };
+
 rawData.forEach(function(projectObject) {
   projects.push(new Project(projectObject));
 });
