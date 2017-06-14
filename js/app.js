0c4405c6fff214d4e1895c0858b6145a4249e1c9
ohjonah <ohjonah@gmail.com>
2017-06-14 12:18:45 -0700

    Populated Dom with Projec Title
    Added forEach statement to populate DOM with instantiated projects

--- js/app.js
+++ js/app.js
@@ -13,35 +13,20 @@ function Project(rawDataObj) {
   this.repoUrl = rawDataObj.repoUrl;
 }
 
-+  $newArticle.removeClass('template');
-    if (!this.publishedOn) $newArticle.addClass('draft');
-    $newArticle.data('category', this.category);
+Project.prototype.toHtml = function() {
+  var $newProject = $('article.template').clone();
 
-   /* TODO: Now use jQuery traversal and setter methods to fill in the rest
-   of the current template clone with properties from this particular Article instance.
-   We need to fill in:
+  $newProject.removeClass('template');
 
-     1. author name,
-     2. author url,
-     3. article title,
-      4. article body, and
-      5. publication date. */
+  $newProject.find('.project-title-overlay h2').html(this.projectName);
 
- -    $newArticle.find('.byline a').html(this.author);
- +  $newArticle.find('.byline a').html(this.author);
- +  $newArticle.find('.byline a').attr('href', this.authorUrl);
- +  $newArticle.find('h1').html(this.title);
- +  $newArticle.find('.article-body').html(this.body);
- +  $newArticle.find('time').attr('datetime', this.publishedOn);
-
-    // Display the date as a relative number of 'days ago'
-    $newArticle.find('time').html('about ' + parseInt((new Date() - new Date(this.publishedOn))/60/60/24/1000) + ' days ago');
-   $newArticle.append('<hr>');
-   return $newArticle;
- };
+  return $newProject;
+};
 
 rawData.forEach(function(projectObject) {
   projects.push(new Project(projectObject));
 });
 
-console.log(projects);
+projects.forEach(function(project) {
+  $('#projects').append(project.toHtml());
+});
