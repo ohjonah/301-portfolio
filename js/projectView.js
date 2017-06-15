bbbe3e350a0f4ab5718c84ece410a71fc889c611
ohjonah <ohjonah@gmail.com>
2017-06-15 12:29:46 -0700

    Adds Home Tab
    Adds home tab to populate DOM when clicked on

--- js/projectView.js
+++ js/projectView.js
@@ -4,17 +4,11 @@ var projectView = {};
 
 projectView.handleMainNav = function() {
   $('.main-nav').on('click', '.tab', function() {
-    $('.hero-container .tab-content .template').hide();
-    // $('.tab-content').hide();
-    // $('.template').hide();
+    $('.hero-container').hide();
+    $('.tab-content').hide();
+    $('.template').hide();
     $('.' + $(this).data('content')).show();
-    console.log($(this).data('content'));
-    // $('.tab-content').hide();
-    // $('.' + $(this).data('content')).fadeIn();
-
   });
-  //
-  // $('.main-nav .tab:first').click(); // Let's now trigger a click on the first .tab element, to set up the page.
 };
 
 projectView.handleMainNav();
