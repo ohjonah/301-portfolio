6d77bfbe2fa86393786c28f1b41638c9de4d03ad
ohjonah <ohjonah@gmail.com>
2017-06-20 10:01:35 -0700

    Adds GMaps API
    Adds google maps api to set location of where I'm located

---
+++ vendor/scripts/map.js
@@ -0,0 +1,11 @@
+function initMap() {
+  var uluru = {lat: 47.6062, lng: -122.3321};
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: uluru
+  });
+  var marker = new google.maps.Marker({
+    position: uluru,
+    map: map
+  });
+}
