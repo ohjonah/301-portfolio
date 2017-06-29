5d9af45fc6ffe3a17de910f6b3c57b7848d87882
ohjonah <ohjonah@gmail.com>
2017-06-29 10:38:52 -0700

    Adds Request Proxy

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
+const requestProxy = require('express-request-proxy');
 
 app.use(express.static('./public'));
 
